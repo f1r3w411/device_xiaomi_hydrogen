7dc3b0752bcad62848826072386a9e268995af11
f1r3w411 <novikov.firewall@gmail.com>
2020-03-31 12:57:08 +0300

    Build fingerprint update

--- bliss_hydrogen.mk
+++ bliss_hydrogen.mk
@@ -38,6 +38,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="hydrogen-user 6.0.1 MMB29M V8.2.3.0.MBCCNDL release-keys"
+    PRIVATE_BUILD_DESC="hydrogen-user 7.0 NRD90M V10.2.2.0.NBCMIXM release-keys"
 
-BUILD_FINGERPRINT := "Xiaomi/hydrogen/hydrogen:6.0.1/MMB29M/V8.2.3.0.MBCCNDL:user/release-keys"
+BUILD_FINGERPRINT := Xiaomi/hydrogen/hydrogen:7.0/NRD90M/V10.2.2.0.NBCMIXM:user/release-keys
