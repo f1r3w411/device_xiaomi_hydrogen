76667297f02a17dd43f4a9164b0aca96809c4fb0
f1r3w411 <novikov.firewall@gmail.com>
2021-01-03 20:37:59 +0200

    MSM related changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,10 +16,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hydrogen.mk \
+    $(LOCAL_DIR)/xtended_hydrogen.mk \
     $(LOCAL_DIR)/full_hydrogen.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_hydrogen-user \
-    lineage_hydrogen-userdebug \
-    lineage_hydrogen-eng
+    xtended_hydrogen-user \
+    xtended_hydrogen-userdebug \
+    xtended_hydrogen-eng
